50e1efe4595afde87b90e5e7e1e05a188085e3b1
Daniel B <danielbarr16@gmail.com>
2019-11-11 11:37:57 -0300

    Creating html pages

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,7 +3,8 @@ from api import views
 
 
 urlpatterns = [
-    path('registrar/categoria', views.CategoriaView.as_view(), name='rgistrar_categoria'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('registrar/categoria', views.CategoriaView.as_view(), name='registrar_categoria'),
     path('registrar/tipo', views.TipoView.as_view(), name='registrar_tipo'),
     path('registrar/Status', views.StatusView.as_view(), name='registrar_status')
 ]
